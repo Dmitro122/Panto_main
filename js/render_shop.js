78bd8db210fb01c6f8479199e078f80ec77ae228
Dmitro122 <106248963+Dmitro122@users.noreply.github.com>
2024-12-12 21:14:07 +0200

    Ол

--- js/render_shop.js
+++ js/render_shop.js
@@ -1,7 +1,7 @@
 let section = document.querySelector(".shop_content")
 
 function fetchCard() {
-	fetch("../data.json")
+	fetch("./data.json")
 		.then(response => response.json())
 		.then(data => {
 			data.cards.forEach(element => {
